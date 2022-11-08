a598bb51ca3c272d2a4b05164cf6e6b13ff3af2b
Riad Shalahin Leon <105214634+riadleon@users.noreply.github.com>
2022-11-08 23:52:41 +0600

    error error

--- src/Pages/DetailService/DetailService.js
+++ src/Pages/DetailService/DetailService.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 
 const DetailService = () => {
-    const { _id, img, price, title, rating, description } = useLoaderData();
+    const { _id, img, title } = useLoaderData();
     return (
         <div>
-            <h3 className='text-6xl text-center font-bold'>{title}</h3>
+            <h3 className='text-6xl text-center font-bold'>Hello: {title}</h3>
+            {/* <img src={img} alt="" /> */}
         </div>
     );
 };
