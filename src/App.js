0aa4a7f67cc3c328fe23b7fe732f7ef918605575
Riad Shalahin Leon <105214634+riadleon@users.noreply.github.com>
2022-11-08 16:55:17 +0600

    added some essentials component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import { Router, RouterProvider } from 'react-router-dom';
+import router from './Router/Routes/Routes'
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
