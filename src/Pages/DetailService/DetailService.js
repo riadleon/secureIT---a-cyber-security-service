54e7aabf67546f39d6d48b4c903286fec45532c0
Riad Shalahin Leon <105214634+riadleon@users.noreply.github.com>
2022-11-09 02:11:28 +0600

    react photoview added

--- src/Pages/DetailService/DetailService.js
+++ src/Pages/DetailService/DetailService.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { FaStar } from 'react-icons/fa';
 import { useLoaderData } from 'react-router-dom';
 import ReviewSection from '../ReviewSection/ReviewSection';
+import { PhotoProvider, PhotoView } from 'react-photo-view';
+import 'react-photo-view/dist/react-photo-view.css';
 
 const DetailService = () => {
     const { _id, img, title, description, price, rating } = useLoaderData();
@@ -9,7 +11,12 @@ const DetailService = () => {
         <div className='p-5 ml-10'>
             <div className='mb-12'>
                 <h3 className='text-6xl  font-bold mb-5 mt-5'>{title}</h3>
-                <img className='w-2/5 h-96 mb-10' src={img} alt="" />
+                <PhotoProvider>
+                    <PhotoView src="/1.jpg">
+                        <img className='w-2/5 h-96 mb-10' src={img} alt="" />
+                    </PhotoView>
+                </PhotoProvider>
+
                 <p className='text-3xl mb-10'> <span className='font-bold'>What is {title}? </span> {description}</p>
                 <div className="text-4xl">
 
