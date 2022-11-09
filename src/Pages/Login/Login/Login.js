e256ebb0ae9b83f3e9d5f2d3d30c20ce7305ae8c
Riad Shalahin Leon <105214634+riadleon@users.noreply.github.com>
2022-11-09 13:28:34 +0600

    login updated

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,18 +1,14 @@
 
-import { GoogleAuthProvider } from 'firebase/auth';
 import React, { useContext, useState } from 'react'
 import toast from 'react-hot-toast';
-import { FaGoogle } from 'react-icons/fa';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../../contexts/AuthProvider/AuthProvider';
-import useTitle from '../../../hooks/useTitle';
 
 
 
 const Login = () => {
-    useTitle('Login-SecureIT')
     const [error, setError] = useState('');
-    const { signIn, setLoading, passReset, providerLogin } = useContext(AuthContext);
+    const { signIn, setLoading, passReset } = useContext(AuthContext);
     const navigate = useNavigate();
     const location = useLocation();
 
@@ -59,59 +55,31 @@ const Login = () => {
             })
             .catch(error => console.error(error))
     }
-    const googleProvider = new GoogleAuthProvider();
-    const handleGoogleSignIn = () => {
-        providerLogin(googleProvider)
-            .then(result => {
-                const user = result.user;
-                console.log(user);
-                toast.success('Google logged in successfully')
-            })
-            .catch(error => console.error(error))
-    }
 
     return (
 
 
-
-        <div className="text-white w-full max-w-md p-4 rounded-md shadow bg-teal-800 mx-auto m-10">
-            <h2 className="mb-3 text-3xl font-semibold text-center text-white">Login to your account</h2>
-            <p className="text-sm text-center text-gray-400">Don't have account?
-                <Link to='/register' className="focus:underline hover:underline">Sign up here</Link>
-            </p>
-            <div className="my-6 space-y-4">
-                <Link ><button onClick={handleGoogleSignIn} type="button" className="flex items-center justify-center w-full p-4 space-x-4 border rounded-md focus:ring-2 focus:ring-offset-1 dark:border-gray-400 focus:ring-purple-400">
-
-                    <FaGoogle className='mr-2'></FaGoogle> Login with Google
-                </button></Link>
-
-            </div>
-            <div className="flex items-center w-full my-4">
-                <hr className="w-full dark:text-gray-400" />
-                <p className="px-3 dark:text-gray-400">OR</p>
-                <hr className="w-full dark:text-gray-400" />
-            </div>
-            <form onSubmit={handleSubmit} className="space-y-8 ng-untouched ng-pristine ng-valid">
-                <div className="space-y-4">
-                    <div className="space-y-2">
-                        <label htmlFor="email" className="block text-sm">Email address</label>
-                        <input type="email" name="email" id="email" placeholder="write your email..." className="w-full px-3 py-2 border rounded-md dark:border-gray-700 bg-gray-900 dark:text-gray-100 focus:dark:border-purple-400" />
-                    </div>
-                    <div className="space-y-2">
-                        <div className="flex justify-between">
-                            <label htmlFor="password" className="text-sm">Password</label>
-                            <Link onClick={handlePasswordChange} className="text-xs hover:underline dark:text-gray-400">Forgot Your Password?</Link>
-                        </div>
-                        <input type="password" name="password" id="password" className="w-full px-3 py-2 border rounded-md dark:border-gray-700 bg-gray-900 dark:text-gray-100 focus:dark:border-purple-400" />
-                    </div>
+        <form onSubmit={handleSubmit}>
+            <div className="section text-center">
+                <h4 className="text-3xl m-4 font-bold">Log In</h4>
+                <div className="pace-y-4">
+                    <input type="email" name="email" className="w-1/5 px-3 py-2 border rounded-md dark:border-gray-700 "
+                        placeholder="Your Email" id="logemail" autocomplete="off" />
+                    <i className="input-icon uil uil-at"></i>
                 </div>
-                <button type="button" className="w-full px-8 py-3 font-semibold rounded-md btn btn-ghost bg-teal-400">Log in</button>
+                <div className="form-group mt-2">
+                    <input type="password" name="password" className="w-1/5 px-3 py-2 border rounded-md dark:border-gray-700 "
+                        placeholder="Your Password" id="logpass" autocomplete="off" />
+                    <i className="input-icon uil uil-lock-alt"></i>
+                </div>
+                <button className="mt-5 px-8 py-3 font-semibold rounded-md btn btn-ghost bg-teal-400">Submit</button>
+                <p className="mb-0 mt-4 text-center"><Link to='/register' className="link">Do not have an account? </Link></p>
+                <p className="mb-0 mt-4 text-center"><Link onClick={handlePasswordChange}>Forgot your password?</Link></p>
                 <p className="mb-0 mt-4 text-center">
                     {error}
                 </p>
-            </form>
-        </div>
-
+            </div>
+        </form>
 
 
     )
