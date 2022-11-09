25074a3d0f06ed4824774c922c5c367494b5c68c
Riad Shalahin Leon <105214634+riadleon@users.noreply.github.com>
2022-11-09 13:42:12 +0600

    header section polished

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -10,6 +10,21 @@ const Header = () => {
             .then(() => { })
             .catch(err => console.error(err))
     }
+    const menuItems = <>
+        <li className='font-semibold'><Link to='/'>Home</Link></li>
+        {
+            user?.email ?
+                <>
+                    <li className='font-semibold'><Link to='/services'>Services</Link></li>
+                    <li className='font-semibold'>
+                        <button onClick={handleLogOut} className='btn-ghost'>Sign Out</button>
+                    </li>
+
+                </>
+                :
+                <li className='font-semibold'><Link to='/login'>Login</Link></li>
+        }
+    </>
     return (
         <div className="navbar bg-color text-white">
             <div className="navbar-start">
@@ -17,8 +32,8 @@ const Header = () => {
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
-                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                        <li>Home</li>
+                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52 bg-teal-600">
+                        {menuItems}
                         {/* <li tabIndex={0}>
                             <a className="justify-between">
                                 Parent
@@ -29,14 +44,14 @@ const Header = () => {
                                 <li><a>Submenu 2</a></li>
                             </ul>
                         </li> */}
-                        <li> <Link to='/services'>Services</Link> </li>
+
                     </ul>
                 </div>
                 <Link className="btn btn-ghost normal-case text-xl" to='/'>SecureIT</Link>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal p-0">
-                    <li><Link to='/'>Home</Link></li>
+                    {menuItems}
                     {/* <li tabIndex={0}>
                         <a>
                             Parent
@@ -47,15 +62,10 @@ const Header = () => {
                             <li><a>Submenu 2</a></li>
                         </ul>
                     </li> */}
-                    <li> <Link to='/services'>Services</Link> </li>
-                    <li> <Link to='/login'>Login</Link> </li>
-                    <li> <Link onClick={handleLogOut}>Logout</Link> </li>
+
                 </ul>
             </div>
-            {/* <div className="navbar-end">
-                <img style={{ height: '30px' }}
-                    roundedCircle src={user.photoURL} alt="" />
-            </div> */}
+
         </div>
     );
 };
